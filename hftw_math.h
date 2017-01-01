d1b9abaa00b1348f1ae4c1fb1def34fadd8c9c55
Dominik Madarász <madaraszd@mail.ru>
2017-01-02 00:26:23 +0100

    Updating Math library, to conform Zakary Strange's updates.

--- hftw_math.h
+++ hftw_math.h
@@ -1,10 +1,13 @@
-// (c) 2016 ZaKlaus; All Rights Reserved
-// Code author: Zakary "zak" Strange
+// (c) 2016 ZaKlaus; All Bs Reserved
+// Original code author: Zakary "zak" Strange
 
 #ifndef HFTW_MATH_H
 #define HFTW_MATH_H
 
-#include <math.h> // TODO(zak): Remove this later on
+// TODO(zaklaus): Implement all OPs for different types.
+
+
+#include <math.h>
 
 #define MathPI32 3.14159265359f
 #define MathPI 3.14159265358979323846f
@@ -33,7 +36,7 @@ typedef union vec2
 
     struct
     {
-        real32 Left, Right;
+        real32 A, B;
     };
 
     real32 Elements[2];
@@ -142,6 +145,27 @@ typedef union vec4
     real32 Elements[4];
 } vec4;
 
+doc(quaternion)
+doc_string(Quaternion. Alias: q4.)
+
+typedef struct
+{
+    struct
+    {
+        union
+        {
+            vec3 XYZ;
+            
+            struct
+            {
+                real32 X, Y, Z;
+            };
+            
+            real32 W;
+        };
+    };
+} quaternion;
+
 doc(mat4)
 doc_string(4x4 Matrix. Alias: m4)
 
@@ -157,7 +181,8 @@ doc_ignore()
 typedef vec2 v2;
 typedef vec3 v3;
 typedef vec4 v4;
-typedef mat4 m4;    
+typedef mat4 m4;
+typedef quaternion q4;
 
 doc_hunt()
 
@@ -168,17 +193,6 @@ internal real32
 MathToRadians(real32 Degrees)
 {
     real32 Result = Degrees * (MathPI32 / 180.0f);
-
-    return (Result);
-}
-
-doc(MathInner)
-doc_string(Calculates scalar value from 2 vectors.)
-
-internal real32
-MathInner(vec3 A, vec3 B)
-{
-    real32 Result = A.X * B.X + A.Y * B.Y + A.Z * B.Z;
 
     return (Result);
 }
@@ -194,13 +208,26 @@ MathSquareRoot(real32 Float)
     return(Result);
 }
 
+doc(MathDot)
+doc_string(Calculates the dot product of 2 vectors.)
+
+internal real32
+MathDot(vec3 VecOne, vec3 VecTwo)
+{
+    real32 Result = 0;
+    
+    Result = (VecOne.X * VecTwo.X) + (VecOne.Y * VecTwo.Y) + (VecOne.Z * VecTwo.Z);
+    
+    return (Result);
+}
+
 doc(MathLengthSquareRoot)
 doc_string(Calculates the length of a square root.)
 
 internal real32
 MathLengthSquareRoot(vec3 A)
 {
-    real32 Result = MathInner(A, A);
+    real32 Result = MathDot(A, A);
 
     return (Result);
 }
@@ -243,22 +270,19 @@ doc_string(Calculates the power of E for value.)
 internal real32
 MathPower(real32 Base, int Exponent)
 {
-    real32 Result = 1;
-    if(Exponent > 0)
+    real32 Result = 1.f;
+    real32 Mul = Exponent < 0 ? 1.f / Base : Base;
+    uint32 X = Exponent < 0 ? -Exponent : Exponent;
+    
+    while(X)
     {
-        int i;
-        for(i = 0; i < Exponent; ++i)
+        if(X & 1)
         {
-            Result *= Base;
+            Result *= Mul;
         }
-    }
-    else
-    {
-        int i;
-        for(i = 0; i > Exponent; --i)
-        {
-            Result /= Base;
-        }
+        
+        Mul *= Mul;
+        X >>= 1;
     }
     return (Result);
 }
@@ -301,10 +325,12 @@ internal vec3
 MathNormalize(vec3 A)
 {
     vec3 Result = {0};
+    
+    real32 Length = MathLength(A);
 
-    Result.X = A.X * (1.0f / MathLength(A));
-    Result.Y = A.Y * (1.0f / MathLength(A));
-    Result.Z = A.Z * (1.0f / MathLength(A));
+    Result.X = A.X * (1.0f / Length);
+    Result.Y = A.Y * (1.0f / Length);
+    Result.Z = A.Z * (1.0f / Length);
     
     return (Result);
 }
@@ -320,19 +346,6 @@ MathCross(vec3 VecOne, vec3 VecTwo)
     Result.X = (VecOne.Y * VecTwo.Z) - (VecOne.Z * VecTwo.Y);
     Result.Y = (VecOne.Z * VecTwo.X) - (VecOne.X * VecTwo.Z);
     Result.Z = (VecOne.X * VecTwo.Y) - (VecOne.Y * VecTwo.X);
-
-    return (Result);
-}
-
-doc(MathDot)
-doc_string(Calculates the dot product of 2 vectors.)
-
-internal real32
-MathDot(vec3 VecOne, vec3 VecTwo)
-{
-    real32 Result = 0;
-
-    Result = (VecOne.X * VecTwo.X) + (VecOne.Y * VecTwo.Y) + (VecOne.Z * VecTwo.Z);
 
     return (Result);
 }
@@ -432,145 +445,169 @@ doc(MathAddVec2)
 doc_string(Adds vector to vec2.)
 
 internal vec2
-MathAddVec2(vec2 Left, vec2 Right)
+MathAddVec2(vec2 A, vec2 B)
 {
     vec2 Result;
 
-    Result.X = Left.X + Right.X;
-    Result.Y = Left.Y + Right.Y;
+    Result.X = A.X + B.X;
+    Result.Y = A.Y + B.Y;
 
     return (Result);
 }
 
 internal vec3
-MathAddVec3(vec3 Left, vec3 Right)
+MathAddVec3(vec3 A, vec3 B)
 {
     vec3 Result;
 
-    Result.X = Left.X + Right.X;
-    Result.Y = Left.Y + Right.Y;
-    Result.Z = Left.Z + Right.Z;
+    Result.X = A.X + B.X;
+    Result.Y = A.Y + B.Y;
+    Result.Z = A.Z + B.Z;
 
     return (Result);
 }
 
 internal vec4
-MathAddVec4(vec4 Left, vec4 Right)
+MathAddVec4(vec4 A, vec4 B)
 {
     vec4 Result;
 
-    Result.X = Left.X + Right.X;
-    Result.Y = Left.Y + Right.Y;
-    Result.Z = Left.Z + Right.Z;
-    Result.W = Left.W + Right.W;
+    Result.X = A.X + B.X;
+    Result.Y = A.Y + B.Y;
+    Result.Z = A.Z + B.Z;
+    Result.W = A.W + B.W;
 
     return (Result);
 }
 
 internal vec2
-MathSubtractVec2(vec2 Left, vec2 Right)
+MathSubtractVec2(vec2 A, vec2 B)
 {
     vec2 Result;
 
-    Result.X = Left.X - Right.X;
-    Result.Y = Left.Y - Right.Y;
+    Result.X = A.X - B.X;
+    Result.Y = A.Y - B.Y;
 
     return (Result);
 }
 
 internal vec3
-MathSubtractVec3(vec3 Left, vec3 Right)
+MathSubtractVec3(vec3 A, vec3 B)
 {
     vec3 Result;
 
-    Result.X = Left.X - Right.X;
-    Result.Y = Left.Y - Right.Y;
-    Result.Z = Left.Z - Right.Z;
+    Result.X = A.X - B.X;
+    Result.Y = A.Y - B.Y;
+    Result.Z = A.Z - B.Z;
 
     return (Result);
 }
 
 internal vec4
-MathSubtractVec4(vec4 Left, vec4 Right)
+MathSubtractVec4(vec4 A, vec4 B)
 {
     vec4 Result;
 
-    Result.X = Left.X - Right.X;
-    Result.Y = Left.Y - Right.Y;
-    Result.Z = Left.Z - Right.Z;
-    Result.W = Left.W - Right.W;
+    Result.X = A.X - B.X;
+    Result.Y = A.Y - B.Y;
+    Result.Z = A.Z - B.Z;
+    Result.W = A.W - B.W;
 
     return (Result);
 }
 
 internal vec2
-MathMultiplyVec2(vec2 Left, vec2 Right)
+MathMultiplyVec2(vec2 A, vec2 B)
 {
     vec2 Result;
 
-    Result.X = Left.X * Right.X;
-    Result.Y = Left.Y * Right.Y;
+    Result.X = A.X * B.X;
+    Result.Y = A.Y * B.Y;
 
     return (Result);
 }
 
 internal vec3
-MathMultiplyVec3(vec3 Left, vec3 Right)
+MathMultiplyVec3(vec3 A, vec3 B)
 {
     vec3 Result;
 
-    Result.X = Left.Z * Right.X;
-    Result.Y = Left.Y * Right.Y;
-    Result.Z = Left.Z * Right.Z;
+    Result.X = A.Z * B.X;
+    Result.Y = A.Y * B.Y;
+    Result.Z = A.Z * B.Z;
 
     return (Result);
 }
 
+internal vec3
+MathMultiplyVec3f(vec3 A, real32 B)
+{
+    vec3 Result;
+    
+    Result.X = A.Z * B;
+    Result.Y = A.Y * B;
+    Result.Z = A.Z * B;
+    
+    return (Result);
+}
+
 internal vec4
-MathMultiplyVec4(vec4 Left, vec4 Right)
+MathMultiplyVec4(vec4 A, vec4 B)
 {
     vec4 Result;
 
-    Result.X = Left.X * Right.X;
-    Result.Y = Left.Y * Right.Y;
-    Result.Z = Left.Z * Right.Z;
-    Result.W = Left.W * Right.W;
+    Result.X = A.X * B.X;
+    Result.Y = A.Y * B.Y;
+    Result.Z = A.Z * B.Z;
+    Result.W = A.W * B.W;
 
     return (Result);
 }
 
 internal vec2
-MathDivideVec2(vec2 Left, vec2 Right)
+MathDivideVec2(vec2 A, vec2 B)
 {
     vec2 Result;
 
-    Result.X = Left.X / Right.X;
-    Result.Y = Left.Y / Right.Y;
+    Result.X = A.X / B.X;
+    Result.Y = A.Y / B.Y;
 
     return (Result);
 }
 
 internal vec3
-MathDivideVec3(vec3 Left, vec3 Right)
+MathDivideVec3(vec3 A, vec3 B)
 {
     vec3 Result;
 
-    Result.X = Left.X / Right.X;
-    Result.Y = Left.Y / Right.Y;
-    Result.Z = Left.Z / Right.Z;
+    Result.X = A.X / B.X;
+    Result.Y = A.Y / B.Y;
+    Result.Z = A.Z / B.Z;
 
     return (Result);
 }
 
+internal vec3
+MathDivideVec3f(vec3 A, real32 B)
+{
+    vec3 Result;
+    
+    Result.X = A.X / B;
+    Result.Y = A.Y / B;
+    Result.Z = A.Z / B;
+    
+    return (Result);
+}
+
 internal vec4
-MathDivideVec4(vec4 Left, vec4 Right)
+MathDivideVec4(vec4 A, vec4 B)
 {
     vec4 Result;
 
-    Result.X = Left.X / Right.X;
-    Result.Y = Left.Y / Right.Y;
-    Result.Z = Left.Z / Right.Z;
-    Result.W = Left.W / Right.W;
+    Result.X = A.X / B.X;
+    Result.Y = A.Y / B.Y;
+    Result.Z = A.Z / B.Z;
+    Result.W = A.W / B.W;
 
     return (Result);
 }
@@ -607,7 +644,7 @@ MathMat4d(real32 Diagonal)
 }
 
 internal mat4
-MathMultiplyMat4(mat4 Left, mat4 Right)
+MathMultiplyMat4(mat4 A, mat4 B)
 {
     mat4 Result = MathMat4();
 
@@ -621,7 +658,7 @@ MathMultiplyMat4(mat4 Left, mat4 Right)
             int CurrentMatrice;
             for(CurrentMatrice = 0; CurrentMatrice < 4; ++CurrentMatrice)
             {
-                Sum += Right.Elements[Rows][CurrentMatrice] * Left.Elements[CurrentMatrice][Columns];
+                Sum += B.Elements[Rows][CurrentMatrice] * A.Elements[CurrentMatrice][Columns];
             }
 
             Result.Elements[Rows][Columns] = Sum;
@@ -652,15 +689,15 @@ MathMultiplyMat4ByVec4(mat4 Matrix, vec4 Vector)
 }
 
 internal mat4
-MathOrthographic(real32 Left, real32 Right, real32 Bottom, real32 Top, real32 Near, real32 Far)
+MathOrthographic(real32 A, real32 B, real32 Bottom, real32 Top, real32 Near, real32 Far)
 {
     mat4 Result = MathMat4d(1.0f);
 
-    Result.Elements[0][0] = 2.0f / (Right - Left);
+    Result.Elements[0][0] = 2.0f / (B - A);
     Result.Elements[1][1] = 2.0f / (Top - Bottom);
     Result.Elements[2][2] = 2.0f / (Near - Far);
 
-    Result.Elements[3][0] = (Left + Right) / (Left - Right);
+    Result.Elements[3][0] = (A + B) / (A - B);
     Result.Elements[3][1] = (Bottom + Top) / (Bottom - Top);
     Result.Elements[3][2] = (Far + Near) / (Near - Far);
 
@@ -689,9 +726,9 @@ MathTranslate(vec3 Translation)
 {
     mat4 Result = MathMat4d(1.0f);
 
-    Result.Elements[0][3] = Translation.X;
-    Result.Elements[1][3] = Translation.Y;
-    Result.Elements[2][3] = Translation.Z;
+    Result.Elements[3][0] = Translation.X;
+    Result.Elements[3][1] = Translation.Y;
+    Result.Elements[3][2] = Translation.Z;
 
     return (Result);
 }
@@ -763,4 +800,212 @@ MathScale(vec3 Scale)
     return (Result);
 }
 
-#endif /* HANDMADE_MATH_H */
+internal quaternion
+MathQuaternion(real32 X, real32 Y, real32 Z, real32 W)
+{
+    quaternion Result = {0};
+    
+    Result.X = X;
+    Result.Y = Y;
+    Result.Z = Z;
+    Result.W = W;
+    
+    return(Result);
+}
+
+internal quaternion
+MathQuaternionFromV4(vec4 Vector)
+{
+    quaternion Result = {0};
+    
+    Result.XYZ = Vector.XYZ;
+    Result.W = Vector.W;
+    
+    return(Result);
+}
+
+internal quaternion
+MathQuaternionFromV3(vec3 Vector, real32 W)
+{
+    quaternion Result = {0};
+    
+    Result.XYZ = Vector;
+    Result.W = W;
+    
+    return(Result);
+}
+
+internal quaternion
+MathAddQuaternion(quaternion A, quaternion B)
+{
+    quaternion Result = {0};
+    
+    Result.X = A.X + B.X;
+Result.Y = A.Y + B.Y;
+Result.Z = A.Z + B.Z;
+Result.W = A.W + B.W;
+    
+    return(Result);
+}
+
+internal quaternion
+MathSubtractQuaternion(quaternion A, quaternion B)
+{
+    quaternion Result = {0};
+    
+    Result.X = A.X - B.X;
+    Result.Y = A.Y - B.Y;
+    Result.Z = A.Z - B.Z;
+    Result.W = A.W - B.W;
+    
+    return(Result);
+}
+
+internal quaternion
+MathMultiplyQuaternion(quaternion A, quaternion B)
+{
+    quaternion Result = {0};
+    
+    Result.X = (A.X * B.W) + (A.Y * B.Z) - (A.Z * B.Y) + (A.W * B.X);
+    Result.Y = (-A.X * B.Z) + (A.Y * B.W) + (A.Z * B.X) + (A.W * B.Y);
+    Result.Z = (A.X * B.Y) - (A.Y * B.X) + (A.Z * B.W) + (A.W * B.Z);
+    Result.W = (-A.X * B.X) - (A.Y * B.Y) - (A.Z * B.Z) + (A.W * B.W);
+    
+    return(Result);
+}
+
+internal quaternion
+MathMultiplyQuaternionf(quaternion A, real32 B)
+{
+    quaternion Result = {0};
+    
+    Result.X = A.X * B;
+    Result.Y = A.Y * B;
+    Result.Z = A.Z * B;
+    Result.W = A.W * B;
+    
+    return(Result);
+}
+
+internal quaternion
+MathDivideQuaternion(quaternion A, quaternion B)
+{
+    quaternion Result = {0};
+    
+    Result.X = A.X / B.X;
+    Result.Y = A.Y / B.Y;
+    Result.Z = A.Z / B.Z;
+    Result.W = A.W / B.W;
+    
+    return(Result);
+}
+
+
+internal quaternion
+MathDivideQuaternionf(quaternion A, real32 B)
+{
+    quaternion Result = {0};
+    
+    Result.X = A.X / B;
+    Result.Y = A.Y / B;
+    Result.Z = A.Z / B;
+    Result.W = A.W / B;
+    
+    return(Result);
+}
+
+internal real32
+MathDotQuaternion(quaternion A, quaternion B)
+{
+    real32 Result = .0f;
+    
+    Result = (A.X * B.X) + (A.Y * B.Y) + (A.Z * B.Z) + (A.W * B.W);
+    
+    return(Result);
+}
+
+internal quaternion
+MathNormalizeQuaternion(quaternion A)
+{
+    quaternion Result = {0};
+    
+    real32 Length = MathSquareRoot(MathDotQuaternion(A, A));
+    Result = MathDivideQuaternionf(A, Length);
+    
+    return(Result);
+}
+
+internal quaternion
+MathSlerp(quaternion A, quaternion B, real32 Time)
+{
+    quaternion Result = {0};
+    
+    real32 CosTheta = MathDotQuaternion(A, B);
+    real32 Angle = cosf(CosTheta);
+    
+    real32 S1 = sinf(1.f - Time * Angle);
+    real32 S2 = sinf(Time * Angle);
+    real32 Is = 1.f / sinf(Angle);
+    
+    A = MathMultiplyQuaternionf(A, S1);
+    B = MathMultiplyQuaternionf(B, S2);
+    
+    Result = MathAddQuaternion(A, B);
+    Result = MathMultiplyQuaternionf(Result, Is);
+    
+    return(Result);
+}
+
+internal mat4
+MathQuaternionToMat4(quaternion A)
+{
+    mat4 Result = {0};
+    Result = MathMat4d(1);
+    
+    quaternion nQuaternion = MathNormalizeQuaternion(A);
+    
+    float XX, YY, ZZ,
+    XY, XZ, YZ,
+    WX, WY, WZ;
+    
+    XX = nQuaternion.X * nQuaternion.X;
+    YY = nQuaternion.Y * nQuaternion.Y;
+    ZZ = nQuaternion.Z * nQuaternion.Z;
+    XY = nQuaternion.X * nQuaternion.Y;
+    XZ = nQuaternion.X * nQuaternion.Z;
+    YZ = nQuaternion.Y * nQuaternion.Z;
+    WX = nQuaternion.W * nQuaternion.X;
+    WY = nQuaternion.W * nQuaternion.Y;
+    WZ = nQuaternion.W * nQuaternion.Z;
+    
+    Result.Elements[0][0] = 1.0f - 2.0f * (YY + ZZ);
+    Result.Elements[0][1] = 2.0f * (XY + WZ);
+    Result.Elements[0][2] = 2.0f * (XZ - WY);
+    
+    Result.Elements[1][0] = 2.0f * (XY - WZ);
+    Result.Elements[1][1] = 1.0f - 2.0f * (XX + ZZ);
+    Result.Elements[1][2] = 2.0f * (YZ + WX);
+    
+    Result.Elements[2][0] = 2.0f * (XZ + WY);
+    Result.Elements[2][1] = 2.0f * (YZ - WX);
+    Result.Elements[2][2] = 1.0f - 2.0f * (XX + YY);
+    
+    return(Result);
+}
+
+internal quaternion
+MathQuaternionFromAxisAngle(vec3 Axis, real32 Angle)
+{
+    quaternion Result = {0};
+    
+    real32 nVec3 = MathSquareRoot(MathDot(Axis, Axis));
+    real32 RotationSine = sinf(Angle / 2.f);
+    vec3 RotatedVector = MathMultiplyVec3f(Axis, RotationSine);
+    
+    Result.W = cosf(Angle / 2.f);
+    Result.XYZ = MathDivideVec3f(RotatedVector, nVec3);
+    
+    return(Result);
+}
+
+#endif
