9f65b308995bae07212c16de10d3317534313ed9
Dominik Madarász <madaraszd@mail.ru>
2017-01-02 20:10:08 +0100

    Project structure change.

--- Makefile
+++
@@ -1,5 +0,0 @@
-CC=clang
-CFLAGS=-std=gnu99 -Wall -Wextra -Werror -Wno-unused-function -Wno-missing-field-initializers -Wno-missing-braces
-
-all:
-	$(CC) $(CFLAGS) test.c -o test
--- updocs.bat
+++ updocs.bat
@@ -1,16 +1,19 @@
 @echo off
+
 echo # Handmade FTW > README.md
 type docs\overview.html >> README.md
 type COPYRIGHT.md >> README.md
 
-pushd codecs
+pushd code\codecs
 call updocs
 popd
 
-pushd formats
+pushd code\formats
 call updocs
 popd
 
 call docs docs.html docs\title.txt docs\overview.html
 REM pageant
-psftp -l zaklaus madaraszd.net -b updocs.scr
+psftp -l zaklaus madaraszd.net -b docs\updocs.scr
+
+move docs.html docs\
